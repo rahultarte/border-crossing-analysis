cdb82d390c9410f7532ae3c1dbc65a49d046ab0b
rahultarte <rahul.tarte@gmail.com>
2019-10-31 22:06:26 -0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+# Use this shell script to compile (if necessary) your code and then execute it. Belw is an example of what might be found in this file if your program was written in Python 3
+python3 src/border_crossing_statistics.py --input input/Border_Crossing_Entry_Data.csv --output output/report.csv
